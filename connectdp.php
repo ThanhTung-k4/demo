56d205b700ee963158e1d87c84bbbb70f475f833
Tran Thanh Tung <ttt1211tb@gmail.com>
2025-04-23 19:25:23 +0700

    moi nhat

--- connectdp.php
+++ connectdp.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "qlsv_ht11");
+$con = mysqli_connect("localhost", "root", "123456", "qlsv_ht11");
 if (!$con)
     die('Kết nối không thành công!');
